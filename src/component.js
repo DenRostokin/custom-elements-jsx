46faa0221474c185c390009a4e4f931c83c9a057
Denis Rostokin <denis.rostokin@osinit.com>
2019-01-26 13:03:16 +0300

    Added children states saving

--- src/component.js
+++ src/component.js
@@ -1,5 +1,17 @@
+import { isCustomElement } from './utils'
+
 class Component extends HTMLElement {
     state = {}
+
+    _childrenState = {}
+
+    get childrenState() {
+        return this._childrenState
+    }
+
+    set childrenState(state) {
+        this._childrenState = { ...this._childrenState, ...state }
+    }
 
     connectedCallback() {
         this.appendChild(this.render())
@@ -11,9 +23,57 @@ class Component extends HTMLElement {
         this.componentWillUnmount()
     }
 
+    _getNestedStatesFrom(children) {
+        return Array.prototype.reduce.call(
+            children,
+            (acc, child) => {
+                let state = { ...acc }
+
+                if (isCustomElement(child)) {
+                    state[child.props.key || child.tagName] = child.state
+                }
+
+                if (child.children.length) {
+                    const childrenState = this._getNestedStatesFrom(
+                        child.children
+                    )
+
+                    state = { ...state, ...childrenState }
+                }
+
+                return state
+            },
+            {}
+        )
+    }
+
+    // mutate children collection
+    _setChildrenState(children) {
+        Array.prototype.forEach.call(children, child => {
+            if (isCustomElement(child)) {
+                child.state =
+                    this.childrenState[child.props.key || child.tagName] || {}
+            }
+
+            if (child.children.length) {
+                this._setChildrenState(child.children)
+            }
+        })
+    }
+
+    _setStateFromPrevChildrenForNew(child) {
+        this.childrenState = this._getNestedStatesFrom(this.children)
+
+        this._setChildrenState([child])
+    }
+
     update = () => {
         if (this.children.length) {
-            this.replaceChild(this.render(), this.children[0])
+            const newChild = this.render()
+
+            this._setStateFromPrevChildrenForNew(newChild)
+
+            this.replaceChild(newChild, this.children[0])
 
             this.componentDidUpdate()
         }
@@ -32,7 +92,7 @@ class Component extends HTMLElement {
     componentWillUnmount() {}
 
     render() {
-        return null
+        return document.createElement('div')
     }
 }
 
